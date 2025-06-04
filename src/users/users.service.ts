cdbaa2183821d9c4565b640241c658a94a405b0a
KING DAVID <otwanemark254@gmail.com>
2025-06-05 02:41:31 +0300

    comprehensive user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,12 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { User } from './interfaces/user.interface';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class UsersService {
@@ -64,5 +69,124 @@ export class UsersService {
 
     this.users.push(newUser);
     return newUser;
+  }
+  /**
+   * @return User[]
+   */
+  findAll(): User[] {
+    return this.users;
+  }
+
+  /**
+   * Get active hotel guest
+   * @return User[]
+   */
+  findActive(): User[] {
+    return this.users.filter((user) => user.isActive);
+  }
+
+  /**
+   * Find by Id
+   * @param id
+   * @return User
+   */
+  findOne(id: number): User {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`Guest with id ${id} not found `);
+    }
+    return user;
+  }
+
+  /**
+   * Find guest by email
+   * @param email
+   * @return User
+   */
+  findByEmail(email: string): User {
+    const user = this.users.find((user) => user.email === email);
+
+    if (!user) {
+      throw new NotFoundException(`Guest with email ${email} not found`);
+    }
+    return user;
+  }
+
+  /**
+   * update guest profile
+   * @param id, updateUserDto
+   * @return User
+   */
+  update(id: number, data: UpdateUserDto): User {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+
+    if (userIndex === 1) {
+      throw new NotFoundException(`Guest with id ${id} not found`);
+    }
+
+    if (data.email) {
+      const existingUser = this.users.find(
+        (user) => user.email === data.email && user.id !== id,
+      );
+
+      if (existingUser) {
+        throw new ConflictException('Another guest with this id exists');
+      }
+    }
+    if (data.checkInDate && data.checkOutDate) {
+      if (new Date(data.checkInDate) >= new Date(data.checkOutDate)) {
+        throw new ConflictException(
+          'Check out date must be before check in date',
+        );
+      }
+    }
+    const updatedUser = {
+      ...this.users[userIndex],
+      data,
+      checkInDate: data.checkInDate
+        ? new Date(data.checkInDate)
+        : this.users[userIndex].checkInDate,
+      checkOutDate: data.checkOutDate
+        ? new Date(data.checkOutDate)
+        : this.users[userIndex].checkOutDate,
+      updatedAt: new Date(),
+    };
+    this.users[userIndex] = updatedUser;
+    return updatedUser;
+  }
+
+  /**
+   * soft delete
+   * @param id
+   * @return message: string
+   */
+  remove(id: number): { message: string } {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === 1) {
+      throw new NotFoundException(`Guest with id ${id} not found`);
+    }
+
+    this.users[userIndex].isActive = false;
+    this.users[userIndex].updatedAt = new Date();
+
+    return {
+      message: `Guest ${this.users[userIndex].name} has checked out successfully`,
+    };
+  }
+  /**
+   * Hard delete {remove from array completely}
+   * @param id: number
+   * @return message: string
+   */
+  delete(id: number): { message: string } {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === 1) {
+      throw new NotFoundException(`Guest with id ${id} not found`);
+    }
+    const deleteUser = this.users.splice(userIndex, 1)[0];
+
+    return {
+      message: `Guest ${deleteUser.name} permanently deleted`,
+    };
   }
 }
